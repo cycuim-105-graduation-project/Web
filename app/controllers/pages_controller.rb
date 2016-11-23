3e8adc1e9ad0e3e01138e4d78e1fefc025767495
Internal <internal@localhost.localdomain>
2016-11-23 18:44:33 +0800

    首頁界面完成 活動總覽頁面完成 活動資訊頁面完成 會員修改頁面完成 名片修改頁面修改中 投票紀錄 歷史訊 票券查詢 新增

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,33 @@ class PagesController <ApplicationController
   end
 
   def about
-
   end
+
+  def activities
+  end
+
+  def apply
+  end
+
+  def membership_index
+  end
+
+  def business_card_own
+  end
+
+  def business_card_friend
+  end
+
+  def message_record
+  end
+
+  def ticket_inquiring
+  end
+
+  def voting_record
+  end
+  
+  def contacts
+  end
+
 end
