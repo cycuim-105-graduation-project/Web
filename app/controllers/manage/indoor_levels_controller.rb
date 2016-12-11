3a46d6654d3956eea1bf77661767a215e95de401
pacywu <mnet2886@gmail.com>
2016-12-11 20:17:48 +0800

    Add views of indoor_level index and indoor_level edit.

--- app/controllers/manage/indoor_levels_controller.rb
+++ app/controllers/manage/indoor_levels_controller.rb
@@ -7,4 +7,8 @@ class Manage::IndoorLevelsController < Manage::BasicResourceController
   def object_params
     params.require(:indoor_level).permit(:name, :place_id)
   end
+
+  def url_after_update
+    url_for(action: :index)
+  end
 end
