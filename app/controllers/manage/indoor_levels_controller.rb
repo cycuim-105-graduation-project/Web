4574d953720908ab776e50ed3c36ca4e59fd762c
Yusheng Li <YushengLi@users.noreply.github.com>
2016-12-11 19:39:51 +0800

    Add view for manage/indoor_levels#new

--- app/controllers/manage/indoor_levels_controller.rb
+++ app/controllers/manage/indoor_levels_controller.rb
@@ -1,10 +1,10 @@
-class Manage::IndoorLevelsController < Manage::BaseResourceController
+class Manage::IndoorLevelsController < Manage::BasicResourceController
   private
   def collection_scope
-    InddorLevel.preload(:place)
+    IndoorLevel.preload(:place)
   end
 
   def object_params
-    params.require(:indoor_level).perimt(:name, :place_id)
+    params.require(:indoor_level).permit(:name, :place_id)
   end
 end
