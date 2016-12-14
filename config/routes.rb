84e0acb12b27905a20f8b580a3706b897a0f7336
TangBoShang <fuji89103@gmail.com>
2016-12-14 07:46:19 -0600

    Feature/member management views (#30)
    * Add devise relative views
    
    - user/sing_up  會員註冊介面與功能套版
    - user/sign_in 會登入介面與功能套版
    - user/password_new 會w 忘記密碼介面與套版
    - user/confirmation_new 重寄驗證密碼具面與套版
    - 新增相關圖片
    - javascript  路更
    
    * 首頁界面完成
    活動總覽頁面完成
    活動資訊頁面完成
    會員修改頁面完成
    名片修改頁面修改中
    投票紀錄 歷史訊 票券查詢 新增
    
    * -新增活動內容圖片
    -新增名片大頭貼圖片
    -刪除原始活動內容模板圖片
    -修改關於我的彈跳式選單
    -活動總覽頁面完成
    -活動申請頁面完成
    -名片修改頁面完成
    -名片查看創建修改中
    -相關聯絡訊息頁面完成
    
    * 新增活動總覽圖片
    新增組員圖片
    新增名片界面圖片
    更改並完成關於我的介紹頁面
    活動頁面嵌入四則圖片與敘述
    活動申請頁面圖片更改
    名片聯絡人頁面完成四則好友
    名片個人介面完成
    名片個人修改界面完成
    首頁新增嵌入四則活動圖片
    歷史訊息頁面完成
    票券頁面完成
    
    * Fix conflict
    
    *  修改conflict
    
    *  刪除各頁面廠商管理
     刪除投票紀錄搜尋
    
    *  刪除各頁面投票管理
     fix conflic
    
    *  減少空格
    
    * update business card

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,19 @@ Rails.application.routes.draw do
 
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'activities', to: 'pages#activities'
+  get 'apply', to: 'pages#apply'
+  get 'membership_index', to: 'pages#membership_index'
+  get 'business_card_own', to: 'pages#business_card_own'
+  get 'business_card_friend', to: 'pages#business_card_friend'
+  get 'business_card_own_edito', to: 'pages#business_card_own_edito'
+  get 'message_record', to: 'pages#message_record'
+  get 'ticket_inquiring', to: 'pages#ticket_inquiring'
+  get 'voting_record', to: 'pages#voting_record'
+  get 'contacts', to: 'pages#contacts'
+
   devise_for :users
+
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
@@ -61,7 +73,7 @@ Rails.application.routes.draw do
       post "checkins/:agenda_id", to: 'checkins#create'
     end
   end
-
+  
   namespace :manage do
     get :dashboard, controller: :pages
     resources :beacons, only: %i(new index) do
